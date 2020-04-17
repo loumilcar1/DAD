4ca2102fbca1a625954f02ed42c37cc1ecb4fba7
gusiihm <58486712+gusiihm@users.noreply.github.com>
2020-04-17 19:16:33 +0200

    fin it 3

---
+++ Iteración3/BBDDv300.sql
@@ -0,0 +1,116 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: lap
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dispositivo`
+--
+
+DROP TABLE IF EXISTS `dispositivo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dispositivo` (
+  `idDispositivo` int NOT NULL AUTO_INCREMENT,
+  `idEdificio` int NOT NULL,
+  PRIMARY KEY (`idDispositivo`),
+  UNIQUE KEY `idDispositivo_UNIQUE` (`idDispositivo`),
+  KEY `dispositivo_edificio_idx` (`idEdificio`),
+  CONSTRAINT `dispositivo_edificio` FOREIGN KEY (`idEdificio`) REFERENCES `edificio` (`idEdificio`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dispositivo`
+--
+
+LOCK TABLES `dispositivo` WRITE;
+/*!40000 ALTER TABLE `dispositivo` DISABLE KEYS */;
+INSERT INTO `dispositivo` VALUES (1,1);
+/*!40000 ALTER TABLE `dispositivo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `edificio`
+--
+
+DROP TABLE IF EXISTS `edificio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `edificio` (
+  `idEdificio` int NOT NULL AUTO_INCREMENT,
+  `direccion` varchar(45) NOT NULL,
+  `dni` varchar(12) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellidos` varchar(45) DEFAULT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `numPlantas` int unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idEdificio`),
+  UNIQUE KEY `idEdificio_UNIQUE` (`idEdificio`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `edificio`
+--
+
+LOCK TABLES `edificio` WRITE;
+/*!40000 ALTER TABLE `edificio` DISABLE KEYS */;
+INSERT INTO `edificio` VALUES (1,'c/reina mercedes','11111111V','GUstavo',NULL,'638064638',4);
+/*!40000 ALTER TABLE `edificio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sensores`
+--
+
+DROP TABLE IF EXISTS `sensores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sensores` (
+  `idSensor` int NOT NULL AUTO_INCREMENT,
+  `idDispositivo` int NOT NULL,
+  `temperatura` float DEFAULT NULL,
+  `humedad` float DEFAULT NULL,
+  `calidad_aire` int DEFAULT NULL,
+  `timestamp` bigint DEFAULT NULL,
+  `location` int DEFAULT '0',
+  PRIMARY KEY (`idSensor`),
+  UNIQUE KEY `idSensor_UNIQUE` (`idSensor`),
+  KEY `Senosres_dispositivo_idx` (`idDispositivo`),
+  CONSTRAINT `Senosres_dispositivo` FOREIGN KEY (`idDispositivo`) REFERENCES `dispositivo` (`idDispositivo`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sensores`
+--
+
+LOCK TABLES `sensores` WRITE;
+/*!40000 ALTER TABLE `sensores` DISABLE KEYS */;
+INSERT INTO `sensores` VALUES (1,1,18,38,1400,1587123268,2),(2,1,34,25,1000,1587120268,1),(3,1,25,10,500,1587103268,2),(4,1,26,80,600,1587023268,1);
+/*!40000 ALTER TABLE `sensores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-17 19:14:17
